17612258e3cf1fa5aa07d2a7593f629223122efc
DougGoncalves <doug.goncalvesf@gmail.com>
2021-02-27 09:23:05 -0300

    Add Swagger

--- api.Dockerfile
+++ api.Dockerfile
@@ -1,4 +1,4 @@
-FROM node:alpine
+FROM node:10-alpine
 
 # diretório alvo
 RUN mkdir -p /usr/src/node-api
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { WinstonModule } from 'nest-winston';
 import { winstonConfig } from './configs/winston.config';
@@ -6,6 +7,15 @@ import { winstonConfig } from './configs/winston.config';
 async function bootstrap() {
   const logger = WinstonModule.createLogger(winstonConfig);
   const app = await NestFactory.create(AppModule, { logger });
+
+  const config = new DocumentBuilder()
+    .setTitle('Users')
+    .setDescription('Users CRUD')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
